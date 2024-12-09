debb980a887337121b5d3174ff2551a70989ea8c
Linus Witte <linus.witte@proton.me>
2024-12-09 10:37:40 +0100

    2024/day9/part1 optimized

--- 2024/day9/bin/main.ml
+++ 2024/day9/bin/main.ml
@@ -20,41 +20,34 @@ let int_at_opt s i =
     None
 
 
-let pop_last seq =
-  let rec pop seq acc =
-    match Sequence.hd seq with
-    | Some hd -> (
-        let tail = Sequence.tl_eagerly_exn seq in
-        match Sequence.hd tail with
-        | Some _ -> pop tail (hd :: acc)
-        | None -> (hd, Sequence.of_list (List.rev acc)))
-    | None -> failwith "Empty sequence"
-  in
-  pop seq []
+let rec fill arr pos last =
+  if pos = last then
+    ()
+  else if last >= Array.length !arr then
+    ()
+  else if !arr.(last) <> -1 then (
+    !arr.(pos) <- !arr.(last);
+    !arr.(last) <- -1)
+  else
+    fill arr pos (last - 1)
 
 
-let compress seq =
-  Sequence.unfold ~init:seq ~f:(fun seq ->
-      match Sequence.hd seq with
-      | Some hd when hd <> -1 -> Some (hd, Sequence.tl_eagerly_exn seq)
-      | Some _ ->
-          let r = pop_last seq |> ref in
-
-          while fst !r = -1 do
-            if snd !r |> Sequence.is_empty then
-              r := (0, Sequence.singleton 0)
-            else
-              r := pop_last (snd !r)
-          done;
-
-          let x, s = !r in
-          Some (x, Sequence.tl_eagerly_exn s)
-      | _ -> None)
+let compress arr =
+  let r = ref arr in
+  let last = Array.length arr - 1 in
+  Array.iteri arr ~f:(fun i a ->
+      match a with
+      | -1 -> fill r i last
+      | _ -> ());
+  !r
 
 
 let check_sum =
-  let open Z in
-  Sequence.foldi ~init:zero ~f:(fun i acc a -> acc + (of_int i * of_int a))
+  Array.foldi ~init:0 ~f:(fun i acc a ->
+      if a = -1 then
+        acc
+      else
+        acc + (i * a))
 
 
 let unfold_files files =
@@ -68,11 +61,9 @@ let unfold_files files =
                  Sequence.init n ~f:(fun _ -> -1)
              in
              (block, i + 1)))
-  |> Sequence.concat
+  |> Sequence.concat |> Sequence.to_array
 
 
 let () =
   let input = InputReader.read_lines ~path:"input" ~f:Fn.id |> List.hd_exn in
-
-  (*let input = "233313312141413140210" in*)
-  input |> unfold_files |> compress |> check_sum |> Z.to_string |> Stdio.printf "Part1: %s\n"
+  input |> unfold_files |> compress |> check_sum |> Stdio.printf "Part1: %i\n"
