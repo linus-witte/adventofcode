7fb1d00acd523e4419d4efc071491497fa841f56
Linus Witte <linus.witte@proton.me>
2025-12-12 15:43:56 +0100

    2025/day02

--- 2025/base/src/lib.rs
+++ 2025/base/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{fs, io};
+
+const INPUT: &'static str = "input";
+
+pub fn read_input<F, T>(f: F) -> io::Result<Vec<T>>
+where
+    F: Fn(&str) -> T,
+{
+    let contents = fs::read_to_string(INPUT)?;
+    let lines: Vec<&str> = contents.lines().collect();
+
+    let mut out: Vec<T> = Vec::new();
+
+    for line in lines {
+        out.push(f(&line));
+    }
+
+    Ok(out)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn read_lines() -> io::Result<Vec<String>> {
+    return read_input(|line| line.to_owned());
 }
--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use std::ops::RangeInclusive;
+
+fn divisors(n: usize) -> Vec<usize> {
+    let mut res = Vec::new();
+
+    for i in 1..n {
+        if n % i == 0 {
+            res.push(i);
+        }
+    }
+    return res;
+}
+
+fn part_a(ranges: Vec<RangeInclusive<u64>>) -> u64 {
+    let mut res = 0;
+    for range in ranges {
+        for id in range {
+            let s = id.to_string();
+            let (left, right) = s.split_at(s.len() / 2);
+
+            if left == right {
+                res += id;
+            }
+        }
+    }
+
+    return res;
+}
+
+fn part_b(ranges: Vec<RangeInclusive<u64>>) -> u64 {
+    let mut res = 0;
+    for range in ranges {
+        for id in range {
+            let s = id.to_string();
+            let divs = divisors(s.len());
+
+            for d in divs {
+                let n_slices = s.len() / d;
+                let slices: Vec<&str> = (0..n_slices).map(|i| &s[i * d..(i + 1) * d]).collect();
+                let all_equal = slices.windows(2).all(|w| w[0] == w[1]);
+
+                if all_equal {
+                    res += id;
+                    break;
+                }
+            }
+        }
+    }
+
+    return res;
+}
+
+fn main() -> std::io::Result<()> {
+    let ranges: Vec<RangeInclusive<u64>> = base::read_lines()?
+        .iter()
+        .flat_map(|line| {
+            line.split(',').map(|s| {
+                let (a, b) = s.split_once('-').unwrap();
+                a.parse::<u64>().unwrap()..=b.parse::<u64>().unwrap()
+            })
+        })
+        .collect();
+
+    println!("{}", part_a(ranges.clone()));
+    println!("{}", part_b(ranges));
+
+    Ok(())
 }
